422bfee4a196317103b4c1de642932fe672dff34
krismandev <krismanpratama@gmail.com>
2022-07-26 16:54:03 +0700

    root parent account

--- devbuilddocker.sh
+++ devbuilddocker.sh
@@ -9,7 +9,7 @@ builddate="$(date '+%Y-%m-%d_%H:%M:%S')"
 echo $builddate
 docker build -t backendbillingdashboard:$ver --build-arg VER=$ver --build-arg BUILDDATE=$builddate .
 docker tag backendbillingdashboard:$ver mygit.imitra.com:5020/backendbillingdashboard:$ver
-docker tag backendbillingdashboard:$ver mygit.imitra.com:5020/backendbillingdashboard:latest
+docker tag backendbillingdashboard:$ver mygit.imitra.com:5020/backendbillingdashboard:devlatest
 docker push mygit.imitra.com:5020/backendbillingdashboard:$ver
-docker push mygit.imitra.com:5020/backendbillingdashboard:latest
+docker push mygit.imitra.com:5020/backendbillingdashboard:devlatest
 docker image prune --filter label=stage=builder --force
